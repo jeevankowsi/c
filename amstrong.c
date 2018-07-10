c2d5e9cb37ad1e5f873664c0a9cf489f45334e32
jeevankowsi <40387754+jeevankowsi@users.noreply.github.com>
2018-07-10 09:26:12 +0000

    Update amstrong.c

--- amstrong.c
+++ amstrong.c
@@ -1,13 +1,10 @@
 #include <stdio.h>
-int main()
+void main()
 {
     int number, originalNumber, remainder, result = 0;
-
     printf("Enter a three digit integer: ");
     scanf("%d", &number);
-
     originalNumber = number;
-
     while (originalNumber != 0)
     {
         remainder = originalNumber%10;
@@ -16,9 +13,11 @@ int main()
     }
 
     if(result == number)
+    {
         printf("%d is an Armstrong number.",number);
+    }
     else
+    {
         printf("%d is not an Armstrong number.",number);
-
-    return 0;
+    }
 }
